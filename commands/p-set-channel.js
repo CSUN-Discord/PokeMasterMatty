8e8aaba26a622b3f3d89f55596950860051fe94a
fahadasat <asatfahad@gmail.com>
2025-01-12 23:17:11 -0700

    refactoring and adding error handling

--- commands/p-set-channel.js
+++ commands/p-set-channel.js
@@ -19,23 +19,35 @@ module.exports = {
     permission: [PermissionsBitField.Flags.Administrator],
 
     /**
+     * Executes the command to set the game channel for the Pokemon game in the server.
+     * It stops any previous game and sets the new game channel.
      *
-     * @param interaction
-     * @returns {Promise<void>}
+     * @param {import("discord.js").Interaction} interaction - The interaction object representing the slash command.
+     * @returns {Promise<void>} - A promise that resolves when the operation is completed.
      */
     async execute(interaction) {
-
         const channelId = interaction.options.getString("channel-id")
 
-        //stop previous game
+        try {
+            // Stop any previous ongoing game and cancel related jobs
+            cancelJob();
+            await pokemonGameFunctions.setPlaying(interaction.guild.id, false);
 
-        cancelJob();
-        await pokemonGameFunctions.setPlaying(interaction.guild.id, false);
+            // Set the new game channel for the server
+            await pokemonGameFunctions.setGameChannel(interaction.guild.id, channelId);
 
-        await pokemonGameFunctions.setGameChannel(interaction.guild.id, channelId);
-        interaction.reply({
-            content: "A game channel has been set. Start the game with: \n```/p-start```",
-            flags: MessageFlags.Ephemeral
-        });
+            // Respond to the user with success message
+            await interaction.reply({
+                content: "A game channel has been set. Start the game with: \n```/p-start```",
+                flags: MessageFlags.Ephemeral
+            });
+        } catch (error) {
+            // Handle any errors and send an error message to the user
+            console.error("Error setting game channel:", error);
+            await interaction.reply({
+                content: "There was an error while setting the game channel. Please try again later.",
+                flags: MessageFlags.Ephemeral
+            });
+        }
     },
 };
